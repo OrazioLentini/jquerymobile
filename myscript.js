2dcb477fb2c75bf1b4dbd1120b9ed033b844c890
Orazio Lentini <oraziio@hotmail.com>
2013-03-05 20:41:17 -0500

    Chp 6.2 youtube grids

--- myscript.js
+++ myscript.js
@@ -1,14 +1,22 @@
 function listVideos(data) {
-  console.log(data);
-	//Title
-	console.log(data.feed.entry[0].title.$t);
+	console.log(data);
 	
-	//Description
-	console.log(data.feed.entry[0].media$group.media$description.$t);
+        var output ='';
+        for (var i = 0; i<data.feed.entry.length; i++) {
+            var title = data.feed.entry[i].title.$t;
 	
-	//Thumbnail
-	console.log(data.feed.entry[0].media$group.media$thumbnail[0].url);
+           // var description = data.feed.entry[i].media$group.media$description.$t;
 	
-	//ID
-	console.log(data.feed.entry[0].id.$t.substring(38));
+            var thumbnail = data.feed.entry[i].media$group.media$thumbnail[0].url;
+	
+            //var id = data.feed.entry[0].id.$t.substring(38);
+            
+            var blocktype = ((i % 2) === 1) ? 'b':'a';
+            
+            output += '<div class="ui-block-' + blocktype + '">';
+	    output += '<h3 class="movietitle">' + title + '</h3>';
+            output += '<img src="' + thumbnail + '" alt="' + title + '" />';
+            output +="</div>";
+        }
+        $('#videolist').html(output);
 }
