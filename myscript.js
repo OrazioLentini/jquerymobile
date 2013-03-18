c852930325d6ddf5839bbd6697ee9924b8ae80ca
Orazio Lentini <oraziio@hotmail.com>
2013-03-18 16:23:57 -0300

    Chp 7.3 - adding a page

--- myscript.js
+++ myscript.js
@@ -36,10 +36,20 @@ function jsonFlickrFeed(data) {
 		var title = data.items[i].title;
 		var link = data.items[i].media.m.substring(0,56);
 		var blocktype = ((i%3)===2) ? 'c':
-				((i%3)===1) ? 'b': 'a';
+				((i%3)===1) ? 'b':
+				'a';
 		output += '<div class="ui-block-' + blocktype + '">';
+		output += '<a href="#showphoto" data-transition="fade" onclick="showPhoto(\'' + link + '\', \'' + title + '\')">';
 		output += '<img src="' + link + '_q.jpg" alt="' + title + '"/>';
+		output += '</a>';
 		output += '</div>';
 	} // go through each photo
 	$('#photolist').html(output);
 } //jsonflickerfeed
+
+function showPhoto(link, title) {
+	var output='<a href="#photos" data-transition="fade">';
+	output += '<img src="' + link + '_b.jpg" alt="' +title + '" />';
+	output += '</a>';
+	$('#myphoto').html(output);
+}
